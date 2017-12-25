1ea6fd459bafecd8642c7743441a05e56f7ac752
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-12-25 13:10:30 +0900

    add mp3 play method

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
 const GoogleHome = require('./index')
 
 let device = new GoogleHome.Connecter('192.168.11.13')
-device.speak('Hello').then(console.log)
+device.playMp3('http://www.hypertrombones.jp/sample/system7.mp3').then(console.log).catch(console.log)
