64ba7248aacb31f660deb01d3954a69387cf232d
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-12-25 23:21:10 +0900

    update Usage

--- exmaple.js
+++
@@ -1,5 +0,0 @@
-const googlehome = require('./index.js')
-
-//googlehome.search().then(console.log)
-let device = new googlehome.Connecter('192.168.11.13')
-  device.speak('Hello')//.then(() => device.stopSpeaker())
